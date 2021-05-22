ef36dec4a3d21d9b80cecc8722bae028015279e7
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-22 22:09:45 +0100

    Started on D3 bar chart

--- src/components/D3Map.js
+++ src/components/D3Map.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./D3.css";
+import BarChart from "./BarChart";
 
 function D3Map() {
   return (
-    <div className="flex">
-      <div className="info">
-        <h4>Title</h4>
-        <p>Body</p>
+    <div>
+      <div className="d3">
+        <BarChart />
       </div>
-      <div className="d3">Add D3 Map</div>
     </div>
   );
 }
