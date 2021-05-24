51169349b1905f4abab01d0ac58e47d26e42b15e
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-24 18:13:36 +0100

    Changed data source

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as d3 from "d3";
 import "./BarChart.css";
+import { csv } from "d3";
 
 // Goal: Use d3 to return a grouped bar chart
 // Separate bars for each last outcome category grouped by crime type
@@ -8,21 +9,19 @@ import "./BarChart.css";
 // x axis = One bar for each "Crime type"
 // y axis = "Last outcome category" summed for each crime type
 
-// Call getData initially, fetch and parse csv to an array of objects
-getData();
+let data;
 
-async function getData() {
-  const response = await fetch("2020-01-city-of-london-street.csv");
-  const data = await response.text();
-  const rows = data.split("\n").slice(1);
-  rows.forEach((el) => {
-    const row = el.split(",");
-    const crime = row[9];
-    const outcome = row[10];
-    // console.log(crime, outcome);
-    // Need variables for sum of crime & sum of outcome and then use them with d3
-  });
-}
+csv("2020-01-city-of-london-street.csv").then((d) => {
+  data = d;
+  console.log(data);
+  let crime = [];
+  let outcome = [];
+
+  for (let i = 0; i < data.length; i++) {
+    crime.push(data[i]["Crime type"]);
+    outcome.push(data[i]["Last outcome category"]);
+  }
+});
 
 function BarChart() {
   return (
