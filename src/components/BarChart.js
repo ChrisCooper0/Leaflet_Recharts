a32ca410fe6dd1f42d79c1cc9ee873646485e99f
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-24 20:47:37 +0100

    Updated BarChart & Home

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -5,28 +5,32 @@ import { csv } from "d3";
 
 // Goal: Use d3 to return a grouped bar chart
 // Separate bars for each last outcome category grouped by crime type
-// Similar to this example: https://bl.ocks.org/bricedev/0d95074b6d83a77dc3ad
+// e.g. https://bl.ocks.org/bricedev/0d95074b6d83a77dc3ad
 // x axis = One bar for each "Crime type"
 // y axis = "Last outcome category" summed for each crime type
 
+// Initialise data variable and parse data using d3's csv function
 let data;
 
 csv("2020-01-city-of-london-street.csv").then((d) => {
   data = d;
-  console.log(data);
-  let crime = [];
-  let outcome = [];
+  let allCrimes = [];
+  let allOutcomes = [];
+  let allCrimesSum = null;
 
   for (let i = 0; i < data.length; i++) {
-    crime.push(data[i]["Crime type"]);
-    outcome.push(data[i]["Last outcome category"]);
+    allCrimes.push(data[i]["Crime type"]);
+    allOutcomes.push(data[i]["Last outcome category"]);
   }
+  allCrimesSum = allCrimes.length;
+
+  console.log(allCrimesSum, allCrimes, allOutcomes);
 });
 
 function BarChart() {
   return (
     <div>
-      <div id="d3">D3 Chart here</div>
+      <div>D3 Map</div>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,14 +11,13 @@ function Home() {
           the code
           <a href="https://github.com/ChrisCooper0/valcri_test"> here</a>.
         </h4>
-        <p>
-          <strong>Note:</strong> I chose to learn and implement D3.js over
-          TypeScript for this app in the interest of time. Below are some
-          stretch goals I will continue to work in my own time.
+        <p class="note">
+          Below are some stretch goals I will continue to work on:
         </p>
         <ul className="stretch-goals">
           <li>- Utilise CSS Modules and update UI</li>
-          <li>- Use react-table for the Leaflet Map data breakdown</li>
+          <li>- Use react-table for the data breakdowns on the Leaflet page</li>
+          <li>- Learn more and implement a d3 grouped-bar chart</li>
           <li>- Refactor to TypeScript</li>
         </ul>
       </div>
