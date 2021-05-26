dd9f563b922622d28c0472a6ed1039bd40c8550e
ChrisCooper0 <christopher.cooper@mail.com>
2021-05-26 13:21:54 +0100

    Added dummy data

--- src/components/Recharts.js
+++ src/components/Recharts.js
@@ -11,80 +11,115 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-// Dummy data based on data.police.uk api data
-// Change Q1 & Q2 values to data from api
+// Dummy data & random values
+let num1 = Math.floor(Math.random() * 1000);
+let num2 = Math.floor(Math.random() * 1000);
+let num3 = Math.floor(Math.random() * 1000);
+let num4 = Math.floor(Math.random() * 1000);
+let largeNum1 = Math.floor(Math.random() * 2500);
+let largeNum2 = Math.floor(Math.random() * 2500);
+let largeNum3 = Math.floor(Math.random() * 2500);
+let largeNum4 = Math.floor(Math.random() * 2500);
 
 const data = [
   {
     name: "Anti-social",
-    Q1: 1234,
-    Q2: 4356,
+    Q1: num1,
+    Q2: num2,
+    Q3: num3,
+    Q4: largeNum3,
   },
   {
     name: "Bicycle Theft",
-    Q1: 5432,
-    Q2: 234,
+    Q1: num4,
+    Q2: num3,
+    Q3: num2,
+    Q4: largeNum3,
   },
 
   {
     name: "Burglary",
-    Q1: 5689,
-    Q2: 5887,
+    Q1: num4,
+    Q2: largeNum2,
+    Q3: num1,
+    Q4: num3,
   },
   {
     name: "Criminal Damage",
-    Q1: 3568,
-    Q2: 2345,
+    Q1: num1,
+    Q2: num2,
+    Q3: num3,
+    Q4: num4,
   },
   {
     name: "Drugs",
-    Q1: 2568,
-    Q2: 3546,
+    Q1: num4,
+    Q2: num3,
+    Q3: largeNum1,
+    Q4: num1,
   },
   {
     name: "Other Theft",
-    Q1: 7890,
-    Q2: 1345,
+    Q1: num3,
+    Q2: largeNum3,
+    Q3: largeNum2,
+    Q4: num2,
   },
   {
     name: "Weapons",
-    Q1: 6546,
-    Q2: 7546,
+    Q1: num4,
+    Q2: num1,
+    Q3: num2,
+    Q4: num3,
   },
   {
     name: "Public Order",
-    Q1: 3345,
-    Q2: 3456,
+    Q1: num2,
+    Q2: num3,
+    Q3: num4,
+    Q4: num1,
   },
   {
     name: "Robbery",
-    Q1: 867,
-    Q2: 7899,
+    Q1: num4,
+    Q2: num3,
+    Q3: largeNum2,
+    Q4: num1,
   },
   {
     name: "Shoplifting",
-    Q1: 3456,
-    Q2: 5256,
+    Q1: largeNum4,
+    Q2: num1,
+    Q3: num4,
+    Q4: num2,
   },
   {
     name: "Theft from person",
-    Q1: 3490,
-    Q2: 8676,
+    Q1: num4,
+    Q2: num1,
+    Q3: num2,
+    Q4: num3,
   },
   {
     name: "Vehicle Crime",
-    Q1: 3490,
-    Q2: 3468,
+    Q1: num1,
+    Q2: num2,
+    Q3: num3,
+    Q4: num4,
   },
   {
     name: "Violent Crime",
-    Q1: 3456,
-    Q2: 4556,
+    Q1: num1,
+    Q2: num3,
+    Q3: num3,
+    Q4: largeNum4,
   },
   {
     name: "Other Crime",
-    Q1: 6789,
-    Q2: 2356,
+    Q1: num1,
+    Q2: num2,
+    Q3: num3,
+    Q4: num4,
   },
 ];
 
@@ -108,8 +143,10 @@ export default class Recharts extends PureComponent {
           <YAxis />
           <Tooltip />
           <Legend verticalAlign="top" height={30} />
-          <Bar dataKey="Q1" stackId="a" fill="#2A8FBE" />
-          <Bar dataKey="Q2" stackId="a" fill="#00C6BF" />
+          <Bar dataKey="Q1" stackId="a" fill="#007E6C" />
+          <Bar dataKey="Q2" stackId="a" fill="#6EC5F7" />
+          <Bar dataKey="Q3" stackId="a" fill="#4EDDA6" />
+          <Bar dataKey="Q4" stackId="a" fill="#2A8FBE" />
         </BarChart>
       </ResponsiveContainer>
     );
